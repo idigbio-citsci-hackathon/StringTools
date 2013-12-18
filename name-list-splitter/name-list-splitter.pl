79ceca8ef82fe328d33e7398ff6e81b04bfee884
Chuck McCallum <mccalluc@yahoo.com>
2013-12-18 12:36:14 -0500

    Handles a few more cases. More counter-examples, too. Tests pass.

--- name-list-splitter/name-list-splitter.pl
+++ name-list-splitter/name-list-splitter.pl
@@ -1,30 +1,14 @@
 #!/usr/bin/perl -w
 use strict;
 
-
-sub list_split {
-  my $in = shift;
-  my @tokens = split /(?<=,)/, $in; # keep comma with preceding.
-  my @out;
-  while (@tokens) {
-    my $token = shift @tokens;
-    if ($token =~ /^\s*jr\.?\s*,?\s*$/i && @out) {
-      $out[scalar @out - 1] .= $token;
-    } else {
-      push @out, $token;
-    }
-  }
-  @out = map {s/,$//;$_} @out;
-  return @out;
-}
-
+### CLI and test runner:
 
 if (!@ARGV) {
   warn "usage:\n $0 'some names'\n";
   my $fail = 0;
   while (<DATA>) {
     chomp;
-    next if /^#/;
+    s/#.*$//;
     next unless /./;
     my @list = split /\s*\|\s*/;
     my $in = shift @list;
@@ -50,6 +34,34 @@ if (!@ARGV) {
   }
 }
 
+### Raisin d'etre:
+
+sub list_split {
+  my $in = shift;
+  $in =~s/^collected by[^\w]*//i;
+  my @tokens = grep {$_} # drop empty tokens
+    split m{
+      (?<=[,;]) | # keep comma with preceding.
+      (?:\s+(?:with|and|&)\s+) |
+      (?:\s+w/)
+    }x, $in; 
+  my @out;
+  while (@tokens) {
+    my $token = shift @tokens;
+    if ($token =~ /^\s*jr\.?\s*,?\s*$/i && @out) {
+      $out[scalar @out - 1] .= $token;
+    } else {
+      push @out, $token;
+    }
+  }
+  @out = map {s/[,;]$//;$_} @out;
+  return @out;
+}
+
+### Test data:
+
+# Note that this is just splits and maps names: no other clean-up is in scope.
+# "det" or "phd" are not touched.
 
 __DATA__
 Stuttgart | Stuttgart
@@ -57,3 +69,20 @@ James D. Ray Jr. | James D. Ray Jr.
 C. Earle Smith, Jr. | C. Earle Smith, Jr.
 Olga Lakela, Jackie Patman | Olga Lakela | Jackie Patman
 James D. Ray Jr., C. Earle Smith, Jr., Olga Lakela, Jackie Patman | James D. Ray Jr. | C. Earle Smith, Jr. | Olga Lakela | Jackie Patman
+D. S. Correll and Helen B. Correll | D. S. Correll | Helen B. Correll
+R. K Godfrey with Angus Gholson | R. K Godfrey | Angus Gholson
+H. Maurushat; V. Sullivan, C. Hudson; D. Wise, R.K. Godfrey | H. Maurushat | V. Sullivan | C. Hudson | D. Wise | R.K. Godfrey
+Collected by: R. K. Godfrey | R. K. Godfrey
+william p adams | william p adams
+A. Gholson Jr w/Wilson Baker | A. Gholson Jr | Wilson Baker
+D. B. Ward, with H. F. Decker | D. B. Ward | H. F. Decker
+R. K. Godfrey (det.) & Richard D. Houk | R. K. Godfrey (det.) | Richard D. Houk
+
+### These don't pass yet:
+
+# Cecil R. Slaughter, Ph.D. | Cecil R. Slaughter, Ph.D.
+# R, Kral & P.L. Redfearn | R, Kral | P.L. Redfearn
+# D. B. & S. S. Ward | D. B. Ward | S. S. Ward
+# R> K> Godfrey with Robt. & John Lazor | R> K> Godfrey | Robt. Lazor | John Lazor
+# R, K, Godfrey | R, K, Godfrey # This was a real transcription!
+# R. K. Godfrrey with Robt. & John Lazor | R. K. Godfrrey | Robt. Lazor | John Lazor
